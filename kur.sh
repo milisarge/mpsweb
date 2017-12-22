9b7e0ed90fafab795fc727e978d57383d601e5ae
milisbir <milisarge@gmail.com>
2017-12-22 18:43:20 +0200

    opt

--- kur.sh
+++ kur.sh
@@ -6,10 +6,10 @@ if [ $1 ];then
 		install -d $_hedef/usr/bin
 		install -d $_hedef/etc/nginx/ssl
 		install -d $_hedef/etc/nginx/conf.d
-		install -d $_hedef/opt
+		install -d $_hedef/opt/mpsweb
 		install -d $_hedef/srv/http/
 		cp -rf http/mpsweb $_hedef/srv/http/
-		cp -rf websocket_server *.sh $_hedef/opt/
+		cp -rf websocket_server *.sh $_hedef/opt/mpsweb/
 		cp mpsweb.servis $_hedef/etc/rc.d/init.d/mpsweb
 		[ -f mpsweb.crt ] && cp mpsweb.crt $_hedef/etc/nginx/ssl/mpsweb.crt
 		[ -f mpsweb.key ] && cp mpsweb.key $_hedef/etc/nginx/ssl/mpsweb.key
